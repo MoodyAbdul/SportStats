dcc5930f76cce4b24db0d7e4d8a0a4b0ac12c189
MoodyAbdul <moody.abdul@gmail.com>
2017-03-26 21:45:41 -0700

    Got database connection working, and created tables

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-/**
- * Created by Ali on 2017-03-26.
- */
+// app.js
 
-var express = require('express');
-var app = express();
+var oracledb = require('oracledb');
 
-// Binding express app to port 3000
-app.listen(3000,function(){
-    console.log('Node server running @ http://localhost:3000')
+oracledb.getConnection({
+     user: "ora_i2a0b",
+     password: "a18986142",
+     connectString: "localhost:1522/ug"
+     }, function(err, connection) {
+     if (err) {
+          console.error(err.message);
+          return;
+     }
+     connection.execute( "CREATE TABLE User_Profile" +
+                          "(managerID  VARCHAR2(30)," +
+                          "name VARCHAR2(30)," +
+                          "PRIMARY KEY (managerID))",
+     [],
+     function(err, result) {
+          if (err) {
+               console.error(err.message);
+               doRelease(connection);
+               return;
+          }
+          console.log(result.metaData);
+          console.log(result.rows);
+          doRelease(connection);
+     });
 });
 
+function doRelease(connection) {
+     connection.release(
+          function(err) {
+               if (err) {console.error(err.message);}
+          }
+     );
+}
