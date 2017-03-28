175606d01275200f336745552c7c8c910707cb82
MoodyAbdul <moody.abdul@gmail.com>
2017-03-27 17:26:36 -0700

    testing an insert and select statement

--- app.js
+++ app.js
@@ -25,22 +25,53 @@ oracledb.getConnection({
           console.error(err.message);
           return;
      }
+
      console.log(sqlFile);
-     for (var i = 0; i < sqlStatements.length; i++){
-     connection.execute(sqlStatements[i],
-     [],
-     function(err, result) {
-          if (err) {
-               console.error(err.message);
-               doRelease(connection);
-               return;
-          }
-          console.log(result.metaData);
-          console.log(result.rows);
-          doRelease(connection);
-     });
-     }
-});
+     /*
+     connection.execute( "CREATE TABLE User_Profile" +
+                              "(managerID  VARCHAR2(30)," +
+                              "name VARCHAR2(30)," +
+                              "PRIMARY KEY (managerID))",
+         [],
+         function(err, result) {
+              if (err) {
+                   console.error(err.message);
+                   doRelease(connection);
+                   return;
+              }
+              console.log(result.metaData);
+              console.log(result.rows);
+              doRelease(connection);
+         });*/
+         connection.execute( "SELECT managerID FROM user_profile",
+                       [],
+                       function(err, result) {
+                            if (err) {
+                                 console.error(err.message);
+                                 doRelease(connection);
+                                 return;
+                            }
+                            console.log(result.metaData);
+                            console.log(result.rows);
+                            doRelease(connection);
+                       });
+
+         connection.execute( "insert into user_profile values +
+                                                     "(moody123, 'moody');",
+                                              [],
+                                              function(err, result) {
+                                                   if (err) {
+                                                        console.error(err.message);
+                                                        doRelease(connection);
+                                                        return;
+                                                   }
+                                                   console.log(result.metaData);
+                                                   console.log(result.rows);
+                                                   doRelease(connection);
+                                              });
+
+
+    });
 
 function doRelease(connection) {
      connection.release(
