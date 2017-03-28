b582c6a75db4a5c98b2093640cae28f56ab463d1
Ali Haghani <alihaghani96@gmail.com>
2017-03-27 19:04:26 -0700

    Added POST call for team search.

--- app.js
+++ app.js
@@ -10,33 +10,38 @@ var connAttrs = {
     user: "ora_i2a0b",
     password: "a18986142",
     connectString: "localhost:1522/ug"
-}
+};
 
 app.use(express.static("public"));
 
+app.post('/searchTeam', function (req, res){
+    var teamName = req.body.teamName;
 
-function searchTeam(teamName, show){
-    console.log('test');
-    oracledb.getConnection(connAttrs, function(err, connection) {
-        if (err) {
-            console.error(err.message);
-            return;
-        }
-        console.log(sqlFile);
-        connection.execute("SELECT " + teamName + " FROM team",
-            [],
-            function(err, result) {
-                if (err) {
-                    console.error(err.message);
+    function searchTeam(teamName){
+        console.log('searchTeam button clicked!');
+        console.log(teamName);
+        oracledb.getConnection(connAttrs, function(err, connection) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            console.log(sqlFile);
+            connection.execute("SELECT " + teamName + " FROM team",
+                [],
+                function(err, result) {
+                    if (err) {
+                        console.error(err.message);
+                        doRelease(connection);
+                        return;
+                    }
+                    console.log(result.metaData);
+                    console.log(result.rows);
                     doRelease(connection);
-                    return;
-                }
-                console.log(result.metaData);
-                console.log(result.rows);
-                doRelease(connection);
-            });
-    });
-}
+                });
+        });
+    }
+
+});
 
 
 
@@ -49,3 +54,4 @@ function doRelease(connection) {
 }
 console.log('end test');
 app.listen(8080);
+exports = module.exports = app;
