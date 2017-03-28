c6fdd42c4971b44248efe3789c080975b95c53b4
Ali Haghani <alihaghani96@gmail.com>
2017-03-28 13:18:15 -0700

    Returns HTML table.

--- app.js
+++ app.js
@@ -50,7 +50,8 @@ app.post('/searchTeam', function (req, res){
                     console.log(result.metaData);
                     console.log(result.rows);
                     res.contentType('application/json').status(200);
-                                    res.send(JSON.stringify(result.rows));
+                    res.send(buildHtmlTable(result.rows));
+                    // res.send(JSON.stringify(result.rows));
                     doRelease(connection);
                 });
         });
@@ -67,6 +68,50 @@ function doRelease(connection) {
         }
     );
 }
+
+var _table_ = document.createElement('table'),
+    _tr_ = document.createElement('tr'),
+    _th_ = document.createElement('th'),
+    _td_ = document.createElement('td');
+
+// Builds the HTML Table out of myList json data from Ivy restful service.
+function buildHtmlTable(arr) {
+    var table = _table_.cloneNode(false),
+        columns = addAllColumnHeaders(arr, table);
+    for (var i=0, maxi=arr.length; i < maxi; ++i) {
+        var tr = _tr_.cloneNode(false);
+        for (var j=0, maxj=columns.length; j < maxj ; ++j) {
+            var td = _td_.cloneNode(false);
+            cellValue = arr[i][columns[j]];
+            td.appendChild(document.createTextNode(arr[i][columns[j]] || ''));
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    return table;
+}
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records
+function addAllColumnHeaders(arr, table)
+{
+    var columnSet = [],
+        tr = _tr_.cloneNode(false);
+    for (var i=0, l=arr.length; i < l; i++) {
+        for (var key in arr[i]) {
+            if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key)===-1) {
+                columnSet.push(key);
+                var th = _th_.cloneNode(false);
+                th.appendChild(document.createTextNode(key));
+                tr.appendChild(th);
+            }
+        }
+    }
+    table.appendChild(tr);
+    return columnSet;
+}
+
 console.log('end test');
 app.listen(8080);
 exports = module.exports = app;
