7d9b6aa15e7560ddf9ebbf98aef07ba77b24806a
Arshdeep Ganda <arshdeep.ganda@alumni.ubc.ca>
2017-03-27 17:52:26 -0700

    reorganized some folders and made server start

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// app.js
 
+var express = require('express');
 var oracledb = require('oracledb');
 var fs = require('file-system');
-
+var app = express();
+var path = require('path');
 var sqlFile;
 var sqlStatements;
 
@@ -15,6 +16,33 @@ fs.readFile("create_tables.sql", function(err, data) {
     sqlStatements = sqlFile.split(",");
 });
 
+var connAttrs = {
+    user: "ora_i2a0b",
+    password: "a18986142",
+    connectString: "localhost:1522/ug"
+}
+
+app.use(express.static("public"));
+
+app.listen(8080);
+
+oracledb.getConnection(connAttrs, function(err, connection) {
+    if (err) {
+        console.error(err.message);
+        return;
+    }
+    connection.execute("SELECT managerID FROM user_profile", [],
+        function(err, result) {
+            if (err) {
+                console.error(err.message);
+                doRelease(connection);
+                return;
+            }
+            console.log(result.metaData);
+            console.log(result.rows);
+            doRelease(connection);
+        });
+});
 
 function searchTeam(teamName, show){
 
@@ -45,68 +73,11 @@ function searchTeam(teamName, show){
 }
 
 
-oracledb.getConnection({
-     user: "ora_i2a0b",
-     password: "a18986142",
-     connectString: "localhost:1522/ug"
-     }, function(err, connection) {
-     if (err) {
-          console.error(err.message);
-          return;
-     }
-
-     console.log(sqlFile);
-     /*
-     connection.execute( "CREATE TABLE User_Profile" +
-                              "(managerID  VARCHAR2(30)," +
-                              "name VARCHAR2(30)," +
-                              "PRIMARY KEY (managerID))",
-         [],
-         function(err, result) {
-              if (err) {
-                   console.error(err.message);
-                   doRelease(connection);
-                   return;
-              }
-              console.log(result.metaData);
-              console.log(result.rows);
-              doRelease(connection);
-         });*/
-         connection.execute( "SELECT managerID FROM user_profile",
-                       [],
-                       function(err, result) {
-                            if (err) {
-                                 console.error(err.message);
-                                 doRelease(connection);
-                                 return;
-                            }
-                            console.log(result.metaData);
-                            console.log(result.rows);
-                            doRelease(connection);
-                       });
-
-        /* connection.execute( "insert into user_profile values +
-                                                     "(moody123, 'moody');",
-                                              [],
-                                              function(err, result) {
-                                                   if (err) {
-                                                        console.error(err.message);
-                                                        doRelease(connection);
-                                                        return;
-                                                   }
-                                                   console.log(result.metaData);
-                                                   console.log(result.rows);
-                                                   doRelease(connection);
-                                              });
-                                              */
-
-
-    });
 
 function doRelease(connection) {
-     connection.release(
-          function(err) {
-               if (err) {console.error(err.message);}
-          }
-     );
+    connection.release(
+        function(err) {
+            if (err) { console.error(err.message); }
+        }
+    );
 }
