6e77c5a11358d22869616e0e3caaf8f391095ece
Arshdeep Ganda <arshdeep.ganda@alumni.ubc.ca>
2017-03-29 16:23:35 -0700

    jade version

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ var connAttrs = {
     password: "a18986142",
     connectString: "localhost:1522/ug"
 };
-
-app.use(express.static("public"));
+app.set('views', './public');
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(res, res) {
+  res.render('index');
+})
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
