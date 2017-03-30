494168b76b566a41fd635ef307b673e52798a5ff
MoodyAbdul <moody.abdul@gmail.com>
2017-03-29 18:03:22 -0700

    fixed division query

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -10,19 +10,26 @@
 
 -- Division Query
 -- Find the team that has played an away game against all other teams
+
 "select teamName " +
 "from team " +
 "where teamID in (select awayteamid " +
                     "from plays " +
                     "where hometeamID in (select team.teamid from team) " +
                     "group by awayteamid " +
-                    "having count(*) = (select count(*) from team))"
+                    "having count(*) = (select (count(*) - 1) from team))"
 
 
+-- Select with multiple rows
 -- Select all matches where team X, has played an away game.
 "select matchID " +
 "from plays " +
 "inner join team on team.teamid=plays.awayteamid " +
 "WHERE team.teamname = " + "'" + teamName + "'",
+
+
+-- Aggregation Query
+"select fname, lname, max(points) " +
+"from player"
 
 
