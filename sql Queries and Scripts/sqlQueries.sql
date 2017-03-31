6071cf763090b90c275ed9925ebc34d354aa0d5f
MoodyAbdul <moody.abdul@gmail.com>
2017-03-31 16:08:23 -0700

    added nested query template

--- sql Queries and Scripts/sqlQueries.sql
+++ sql Queries and Scripts/sqlQueries.sql
@@ -48,7 +48,7 @@
 -- Nested Aggregation Query
 -- Find the team which has the highest (MAX) average (AVG) homescore across their match history
 
-select temp.teamname, temp.
+select temp.teamname, temp.anyVariable
 
 -- Find the team which has the lowest (MIN) average (AVG) homescore across their match history
 
