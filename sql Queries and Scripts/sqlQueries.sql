d7e2ec3b737f0426920edf7e076bb87ce5f8ae46
MoodyAbdul <moody.abdul@gmail.com>
2017-03-30 22:32:47 -0700

    aggregation queries

--- sql Queries and Scripts/sqlQueries.sql
+++ sql Queries and Scripts/sqlQueries.sql
@@ -1,4 +1,5 @@
 -- Join Query
+-- Selects the name of the manager belonging to team 'teamName'
 "SELECT fname, lname "
                        + "FROM managers "
                        + "INNER JOIN team ON team.teamID=managers.teamID "
@@ -6,6 +7,7 @@
 
 
 -- Selection & Projection Query
+-- selects the teamid of the given teamName
 "SELECT teamID FROM team WHERE teamname=" + "'" + teamName + "'",
 
 -- Division Query
@@ -29,8 +31,28 @@
 
 
 -- Aggregation Query
-"select fname, lname, max(points) " +
-"from player"
-"working on this"
+-- Finds the player with the max (any variable in stats)
+"select playerid from stats where points = (select max("+ aggregateVar +") from stats)"
 
+-- Selects the players' first and last name who has the lowest (any variables) in the stats table. (Join and Aggregation)
+"select fname, lname from player inner join stats on stats.playerid=player.playerid where points "+
+"= (select min("+ aggregateVar +") from stats)"
+
+-- count the number of players on a given team
+"select count(playerid) from player inner join team on team.teamid=player.teamid " +
+"where team.teamname = " + "'" + teamName + "' " +
+"group by player.teamid"
+
+
+
+-- Nested Aggregation Query
+-- Find the team which has the highest (MAX) average (AVG) homescore across their match history
+
+
+--Find the team with the most players on it.
+"select temp.teamname " +
+"from (select teamid, count(playerid) as countofPlayers " +
+        "from player group by teamid) temp " +
+"inner join temp on temp.teamid = team.teamid "
+"where temp.countofPlayers = (select Max(temp.countofPlayers) from temp)"
 
